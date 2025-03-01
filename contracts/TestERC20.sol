09beb7aa1983bf9bab83e997c5ffcfea98b46897
Shubham Kukreti <57281769+KukretiShubham@users.noreply.github.com>
2025-03-01 07:52:02 -0700

    lint

--- contracts/TestERC20.sol
+++ contracts/TestERC20.sol
@@ -5,13 +5,13 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import {Initializable} from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 
 contract TestERC20 is ERC20, Initializable {
-    constructor(address preFundAddress) ERC20("Test Token", "TTK") {
-        // Prefund the provided address with 100,000 tokens
-        _mint(preFundAddress, 100000 * (10 ** decimals()));
-    }
-    
-    // Open mint function: anyone can mint tokens to any address
-    function mint(address to, uint256 amount) external {
-        _mint(to, amount);
-    }
+	constructor(address preFundAddress) ERC20("Test Token", "TTK") {
+		// Prefund the provided address with 100,000 tokens
+		_mint(preFundAddress, 100000 * (10 ** decimals()));
+	}
+
+	// Open mint function: anyone can mint tokens to any address
+	function mint(address to, uint256 amount) external {
+		_mint(to, amount);
+	}
 }
--- scripts/deploy-example.ts
+++ scripts/deploy-example.ts
@@ -4,14 +4,17 @@ import { ethers, upgrades } from 'hardhat'
 
 async function main() {
 	const testERC20Contract = await ethers.getContractFactory('TestERC20')
-	const testERC20 = await testERC20Contract.deploy('0x4042a17D26436fcbb78B99bB19F4055547D3EB43')
-	await testERC20.waitForDeployment()
-	const testERC20Address = await testERC20.getAddress().then(a => a)
-	console.log({testERC20Address})
-	const tenDollarContract = await ethers.getContractFactory('TenDollarJob')
-	const tenDollar = await upgrades.deployProxy(tenDollarContract,
-		[testERC20Address, '0x4042a17D26436fcbb78B99bB19F4055547D3EB43']
+	const testERC20 = await testERC20Contract.deploy(
+		'0x4042a17D26436fcbb78B99bB19F4055547D3EB43',
 	)
+	await testERC20.waitForDeployment()
+	const testERC20Address = await testERC20.getAddress().then((a) => a)
+	console.log({ testERC20Address })
+	const tenDollarContract = await ethers.getContractFactory('TenDollarJob')
+	const tenDollar = await upgrades.deployProxy(tenDollarContract, [
+		testERC20Address,
+		'0x4042a17D26436fcbb78B99bB19F4055547D3EB43',
+	])
 	await tenDollar.waitForDeployment()
 	console.log('Deployed address:', await tenDollar.getAddress())
 }
